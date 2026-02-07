5eae93c269f654f07ff23afffc202286d577d6ef
natuyade <natuyade@tuta.io>
2026-02-07 23:22:52 +0900

    change output answer system

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
-use fastrand::Rng;
-
 fn main() {
     // vec![値;個数] [1;3]->[1,1,1]
+    // 配列数とwidthでいくらでも変形可能に
     let mut onejigen: Vec<i32> = vec![0; 100];
     let mut row = 0;
     let width = 10;
     let mut column = 0;
     let mut zahyou = 0;
-    let mut rowview;
+    //let mut rowview;
+    let mut kotae;
 
     for _ in 0..onejigen.len() {
-        //onejigen[zahyou] += 1;
+        // onejigen[zahyou] += 1;
         if row!=0&&row!=onejigen.len()/width-1&&column!=0&&column!=width-1{
+            // fastrandで確率実装
             if fastrand::i32(0..100) <= 80{
                 onejigen[zahyou] += 1;
             } else {
                 onejigen[zahyou] += 2;
             }
         } 
-        rowview = &onejigen[row*width..=zahyou];
+        //rowview = &onejigen[row*width..=zahyou];
         column += 1;
         if column == width {
-            println!("{:?}",rowview);
+            //println!("{:?}",rowview);
             row += 1;
             column = 0;
         }
         zahyou = row * width + column;
     }
+    for i in 0..width {
+        kotae = &onejigen[width*i..width*i+width];
+        println!("{:?}",kotae)
+    }
+    
 }
